6b6097bb7f4acc817238248727d35dfd8a4162e6
Gabriel De Lamo <gabrieldelamodutra@hotmail.es>
2022-04-19 19:55:06 +0200

    Añadida mostrar una tabla para poder trabajar con ella

--- client/src/components/Tables.vue
+++ client/src/components/Tables.vue
@@ -2,40 +2,101 @@
     import TablesDataService from '@/services/TablesDataService';
 
     export default {
-        
-
         name: "Tables",
 
-    data(){
-        return{
-            tables: []
-        };
-    },
+        data(){
+            return{
+                candidateTable: [],
+                referenceTable: [],
+                columns:[],
+                row_data:[]
+            };
+        },
 
-    methods: {
-        retrieveTable(){
-            TablesDataService.getTable()
-                .then((response) => {
-                    this.tables = response.data
-                })
+        methods: {
+            GetKeys(Table){
+                return this.colums = Object.keys(Table);
+            },
+
+            retrieveReferenceTable(){
+                TablesDataService.getTable()
+                    .then((response) => {
+                        this.referenceTable = response.data
+                    })
+            },
+            retrieveCandidateTable(){
+                TablesDataService.getTable()
+                    .then((response) => {
+                        if(response.data._id == this.candidateTable._id){
+                            this.retrieveCandidateTable();
+                        }else{
+                            this.candidateTable = response.data
+                        }
+                    });
+            },
+            Reference_Users(){
+                TablesDataService.jsonPlaceHolderUsers()
+                    .then((response) => {
+                        this.referenceTable = response.data
+                        this.columns = Object.keys(this.referenceTable[0]);
+
+                    });
+            },
+            Candidate_Albums(){
+                TablesDataService.jsonPlaceHolderAlmbums()
+                    .then((response) => {
+                        this.candidateTable = response.data 
+                    });
+                }
+            },
+     
+
+        mounted(){
+            this.Reference_Users();
+            this.Candidate_Albums();
+          
+            // this.retrieveReferenceTable();
+            // this.retrieveCandidateTable();
+            console.log(this.candidateTable);
+            console.log(this.referenceTable);
         }
-    },
-
-    mounted(){
-        this.retrieveTable();
-    }
 
     }
    
-
 </script>
 <template>
-asdf
+
+    <div class="container_center">
+   
+ 
     <table>
-        
+        <th v-for="(value,key) in columns">{{value}}</th>
+            <tr v-for="(value,key) in referenceTable">
+                <td v-for="(value2,key) in value">{{value2}}</td> 
+            </tr>
     </table>
     
 
 
 
+    </div>
+
+ 
+
+
+
 </template>
+
+
+<style>
+
+.container_center{
+    height: 100%;
+    width: 100%;
+    min-height: 100%;
+
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
+</style>
--- client/src/services/TablesDataService.js
+++ client/src/services/TablesDataService.js
@@ -1,14 +1,23 @@
 const API_URL = 'http://localhost:3000/random';
+const jsonPlaceHolderAlmbums = 'https://jsonplaceholder.typicode.com/albums'
+const jsonPlaceHolderUsers = 'https://jsonplaceholder.typicode.com/users'
+
 import axios from 'axios'; //PARA EL XACCESSTOKEN
 
 
-class TableDataService{
 
+class TableDataService{
     getTable(){
         return axios.get(API_URL);
     }
 
-}
+    jsonPlaceHolderAlmbums(){
+        return axios.get(jsonPlaceHolderAlmbums);
+    }
 
+    jsonPlaceHolderUsers(){
+        return axios.get(jsonPlaceHolderUsers);
+    }
+}
 
 export default new TableDataService();
