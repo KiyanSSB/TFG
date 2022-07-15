68f5789561345e2b510fe168ff7798e180815a63
Gabriel De Lamo <gabrieldelamodutra@hotmail.es>
2022-07-15 18:54:53 +0200

    Colo

--- client/src/App.vue
+++ client/src/App.vue
@@ -25,7 +25,11 @@ const UserStore = useUserStore()
 
 #app {
   min-height: 100vh;
-  background: linear-gradient(107.56deg, #C2E9FB 0%, rgba(161, 196, 253, 0) 100%);
+  /* background: linear-gradient(107.56deg, #C2E9FB 0%, rgba(161, 196, 253, 0) 100%); */
+
+background: rgb(240,255,250);
+background: linear-gradient(284deg, rgba(240,255,250,1) 13%, rgba(199,255,236,1) 36%, rgba(161,243,251,1) 63%, rgba(223,249,255,1) 82%);
+
 }
 
 .center{
