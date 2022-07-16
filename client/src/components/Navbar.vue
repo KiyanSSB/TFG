7038a3488dbc7d22a96b56b12b6f51b212153eef
Gabriel De Lamo <gabrieldelamodutra@hotmail.es>
2022-07-16 05:22:05 +0200

    Añadido botón para deslogueado

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -34,10 +34,15 @@ const userStore = useUserStore()
             </div>
         </div>
 
-
-        <button v-on:click="userStore.signOutUser()" v-if="userStore.logged != false">
-            {{userStore.userData.email}}
-        </button>
+        <div class="dropdown" style="margin-right: 2%;" v-if="userStore.logged != false">
+            <button class="btn btn-secondary dropdown-toggle btn-info dropy" type="button" id="dropdownMenuButton1"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                {{ userStore.userData.email }}
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" v-on:click="userStore.signOutUser()" >Sign Out</a></li>
+            </ul>
+        </div>
 
 
 
@@ -46,12 +51,14 @@ const userStore = useUserStore()
     </nav>
 </template>
 <style>
-
 a:hover,
 a.router-link-active {
     border-bottom: red solid 2px;
 }
 
+.dropy{
+    border: 1px solid black;
+}
 
 .fade-enter-from,
 .fade-leave-to {
@@ -64,45 +71,42 @@ a.router-link-active {
 }
 
 @property --myColor1 {
-  syntax: '<color>';
-  initial-value: transparent;
-  inherits: false;
+    syntax: '<color>';
+    initial-value: transparent;
+    inherits: false;
 }
 
 @property --myColor2 {
-  syntax: '<color>';
-  initial-value: transparent;
-  inherits: false;
+    syntax: '<color>';
+    initial-value: transparent;
+    inherits: false;
 }
 
 @property --myColor3 {
-  syntax: '<color>';
-  initial-value: transparent;
-  inherits: false;
+    syntax: '<color>';
+    initial-value: transparent;
+    inherits: false;
 }
 
 @property --myColor4 {
-  syntax: '<color>';
-  initial-value: transparent;
-  inherits: false;
+    syntax: '<color>';
+    initial-value: transparent;
+    inherits: false;
 }
 
 
 
-.navbar{
-  background: rgb(245,245,249);
-  background: linear-gradient(127deg, var(--myColor2) , var(--myColor1) , var(--myColor3) , var(--myColor4) );
+.navbar {
+    background: rgb(245, 245, 249);
+    background: linear-gradient(127deg, var(--myColor2), var(--myColor1), var(--myColor3), var(--myColor4));
     /* background: linear-gradient(127deg, var(--myColor1), var(--myColor2)); */
-  transition: --myColor1 2s ease-in , myColor2 3s ease-in , myColor3 15s ease-in-out , myColor4 5s ease-in-out;
+    transition: --myColor1 2s ease-in, myColor2 3s ease-in, myColor3 15s ease-in-out, myColor4 5s ease-in-out;
 }
 
-.navbar:hover{
-  --myColor1: rgba(245,245,249,1) ;
-  --myColor2: rgb(234, 238, 249);
-  --myColor3: #89CADD;
-  --myColor4: #9face6;
+.navbar:hover {
+    --myColor1: rgba(245, 245, 249, 1);
+    --myColor2: rgb(234, 238, 249);
+    --myColor3: #89CADD;
+    --myColor4: #9face6;
 }
-
-
-
 </style>
